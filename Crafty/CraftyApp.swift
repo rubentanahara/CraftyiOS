29a6ac631bf29aca0ed5e74fb0570b66662a3d99
Ruben Tanahara <tanaharaguy@gmail.com>
2024-04-07 09:59:56 -0600

    app

---
+++ Crafty/CraftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CraftyApp.swift
+//  Crafty
+//
+//  Created by Ruben Tanahara on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct CraftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
